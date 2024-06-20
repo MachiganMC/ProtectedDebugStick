7c67f2e17a1a1eab6a0ac35ccc9469c38cbdf028
MachiganMC <xnagos@gmail.com>
2024-06-21 01:45:24 +0200

    chore: change way to detect version
    In 1.20.5, the name of to get Minecraft version has changed.

--- plugin/src/main/java/be/machigan/protecteddebugstick/property/PropertyChooser.java
+++ plugin/src/main/java/be/machigan/protecteddebugstick/property/PropertyChooser.java
@@ -2,18 +2,35 @@ package be.machigan.protecteddebugstick.property;
 
 import be.machigan.protecteddebugstick.PropertyHandler;
 import be.machigan.protecteddebugstick.ProtectedDebugStick;
+import com.google.common.primitives.Ints;
+import org.bukkit.Bukkit;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PropertyChooser {
     public static PropertyHandler getPropertyHandler() {
-        String serverPackage = ProtectedDebugStick.getInstance().getServer().getClass().getPackage().getName();
-        String serverVersion = serverPackage.substring(serverPackage.lastIndexOf('.') + 1);
         try {
-            Class<?> implementation = Class.forName("be.machigan.protecteddebugstick.Property_" + serverVersion);
+            Class<?> implementation = Class.forName("be.machigan.protecteddebugstick.Property_" + getCurrentVersion());
             if (PropertyHandler.class.isAssignableFrom(implementation)) {
                 return (PropertyHandler) implementation.getConstructor().newInstance();
             }
         } catch (Exception ignore) {}
         return null;
+    }
+
+    private static String getCurrentVersion() {
+        Matcher matcher = Pattern.compile("(?<version>\\d+\\.\\d+)(?<patch>\\.\\d+)?").matcher(Bukkit.getBukkitVersion());
+        StringBuilder stringBuilder = new StringBuilder();
+        if (matcher.find()) {
+            stringBuilder.append("v").append(matcher.group("version").replace(".", "_"));
+            String patch = matcher.group("patch");
+            if (patch == null)
+                stringBuilder.append("_R0");
+            else
+                stringBuilder.append(patch.replace(".", "_R"));
+        }
+        return stringBuilder.toString();
     }
 
     private PropertyChooser() {}
